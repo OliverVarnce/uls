ce5f5b0f89cf5827e3254812aa5fd0271490e0ae
Oleh Voitenko <voytenko.ov@gmail.com>
2020-03-11 21:54:22 +0200

    Andrii

--- inc/uls.h
+++ inc/uls.h
@@ -7,17 +7,17 @@
 #define MX_MINOR(dev)      ((unsigned int) ((dev) & MX_MINORMASK))
 
 #define MX_RED  "\33[31m"
-#define MX_YELLOW  "\33[33m"
-#define MX_PINK  "\33[35m"
+#define YELLOW  "\33[33m"
+#define PINK  "\33[35m"
 #define MX_AQUA  "\33[36m"
 #define MX_BLUE  "\33[34m"
-#define MX_GREEN "\33[32m"
-#define MX_YELLOW_BLUE  "\33[43m\33[34m"
-#define MX_AQUA_BLUE  "\33[46m\33[34m"
-#define MX_GREEN_BLACK "\33[42m\33[30m"
+#define GREEN "\33[32m"
+#define BLUE  "\33[43m\33[34m"
+#define AQUA_BLUE  "\33[46m\33[34m"
+#define GREEN_BLACK "\33[42m\33[30m"
 #define MX_BLACK_RED "\33[30m\33[41m"
 #define MX_AQUA_BLACK "\33[46m\33[30m"
-#define MX_YELLOW_BLACK "\33[43m\33[30m"
+#define YELLOW_BLACK "\33[43m\33[30m"
 #define MX_DEFAULT  "\33[0m"
 
 #include "./libmx/inc/libmx.h"
@@ -55,7 +55,7 @@ typedef struct s_flags {
     bool flag_c;
     bool flag_S;
     bool flag_t;
-}t_flags;
+} t_flags;
 
 typedef struct s_column_size {
     int col_one_size;
@@ -69,16 +69,16 @@ typedef struct s_column_size {
     int cur_col_two_size;
     int cur_col_three_size;
     int cur_col_four_size;
-}t_col_size;
+} t_col_size;
 
 typedef struct s_node {
     t_list_dir *ptr;
-}t_node;
+} t_node;
 
 typedef struct s_table {
     int cols;
     int rows;
-}t_table_info;
+} t_table_info;
 
 typedef void (*fptr)(t_list_dir *first, t_list_dir *second, t_flags *opts);
 
--- src/mx_out_G.c
+++ src/mx_out_G.c
@@ -1,7 +1,31 @@
 #include "uls.h"
 
-static void print_reg(char *name, mode_t value);
-static void print_dir(char *name, mode_t value);
+void print_reg(char *name, mode_t value){
+    if(((value & 0x040) && (value & 0x800))
+       || (!(value & 0x040) && (value & 0x800)))
+        mx_add_color(name, MX_BLACK_RED);
+    else if(value & 0x040 && !(value & 0x800)
+            && (((value & 0x008) && (value & 0x400))
+                || (!(value & 0x008) && (value & 0x400))))
+        mx_add_color(name, MX_AQUA_BLACK);
+    else if((value & 0x040 && !(value & 0x800))
+            || (value & 0x008 && !(value & 0x400))
+            || (value & 0x001 && !(value & 0x200))
+            || (value & 0x001 && value & 0x200))
+        mx_add_color(name, MX_RED);
+    else
+        mx_add_color(name, MX_DEFAULT);
+}
+
+void print_dir(char *name, mode_t value){
+    if((value & 0x002) &&((value & 0x001 && value & 0x200)
+                          || (value & 0x001 && value & 0x200)))
+        mx_add_color(name, GREEN_BLACK);
+    else if(value & 0x002 && (value & 0x001 && !(value & 0x200)))
+        mx_add_color(name, YELLOW_BLACK);
+    else
+        mx_add_color(name, MX_BLUE);
+}
 
 void mx_add_color(char *name, char *col){
     mx_printstr(col);
@@ -10,48 +34,22 @@ void mx_add_color(char *name, char *col){
 }
 
 void mx_out_G(char *name, mode_t value) {
-    switch (mx_get_file_type(value)) {
-        case 'p': mx_add_color(name, MX_YELLOW);
-            break;
-        case 'c': mx_add_color(name, MX_YELLOW_BLUE);
-            break;
-        case 'd': print_dir(name, value);
-            break;
-        case 'b': mx_add_color(name, MX_AQUA_BLUE);
-            break;
-        case '-': print_reg(name, value);
-            break;
-        case 'l': mx_add_color(name, MX_PINK);
-            break;
-        case 's': mx_add_color(name, MX_GREEN);
-            break;
-        default: mx_printstr(name);
-    }
+    char color = mx_get_file_type(value)
+    if (color == 'p')
+        mx_add_color(name, YELLOW);
+    else if (color == 'c')
+        mx_add_color(name, YELLOW);
+    else if (color == 'd')
+        print_dir(name, value);
+    else if (color == 'b')
+        mx_add_color(name, AQUA_BLUE);
+    else if (color == '-')
+        print_reg(name, value);
+    else if (color == 'l')
+        mx_add_color(name, PINK);
+    else if (color == 's')
+        mx_add_color(name, GREEN);
+    mx_printstr(name);
+
 }
 
-static void print_reg(char *name, mode_t value){
-    if(((value & 0x040) && (value & 0x800)) 
-    || (!(value & 0x040) && (value & 0x800)))
-        mx_add_color(name, MX_BLACK_RED);
-    else if(value & 0x040 && !(value & 0x800) 
-    && (((value & 0x008) && (value & 0x400)) 
-    || (!(value & 0x008) && (value & 0x400))))
-        mx_add_color(name, MX_AQUA_BLACK);
-    else if((value & 0x040 && !(value & 0x800)) 
-    || (value & 0x008 && !(value & 0x400)) 
-    || (value & 0x001 && !(value & 0x200)) 
-    || (value & 0x001 && value & 0x200))
-        mx_add_color(name, MX_RED);
-    else 
-        mx_add_color(name, MX_DEFAULT);
-}
-
-static void print_dir(char *name, mode_t value){
-    if((value & 0x002) &&((value & 0x001 && value & 0x200) 
-    || (value & 0x001 && value & 0x200)))
-        mx_add_color(name, MX_GREEN_BLACK);
-    else if(value & 0x002 && (value & 0x001 && !(value & 0x200)))
-        mx_add_color(name, MX_YELLOW_BLACK);
-    else
-        mx_add_color(name, MX_BLUE);
-}
