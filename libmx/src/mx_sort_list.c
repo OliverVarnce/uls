d107874ce4d457be1bd30b7779429cf18d442c8f
Oleg Voytenko <voytenko.ov@gmail.com>
2020-04-29 17:15:56 +0300

    fix sort_list

--- libmx/src/mx_sort_list.c
+++ libmx/src/mx_sort_list.c
@@ -1,21 +1,16 @@
 #include "libmx.h"
 
 t_list *mx_sort_list(t_list *lst, bool (*cmp)(void *, void *)) {
-	int i = 0;
-	int size = mx_list_size(lst);
-	t_list *new = lst;
+    if (!lst) return NULL;
 
-	while (i < size) {
-		if (cmp(lst->data, lst->next->data)) {
-			mx_swap_elem(&(lst->data), &(lst->next->data));
-		}
-		lst = lst->next;
-		i++;
-		if (i == size - 1) {
-			lst = new;
-			i = 0;
-			size--;
-		}
-	}
-	return new;
+    for (t_list *i = lst; i != NULL; i = i->next) {
+        for (t_list *j = i->next; j != NULL; j = j->next) {
+            if (cmp(i->data, j->data)) {
+                void *temp = i->data;
+                i->data = j->data;
+                j->data = temp;
+            }
+        }
+    }
+    return lst;
 }
